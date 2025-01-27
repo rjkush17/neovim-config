17bd03b9bfc7853705b0ae747b3b6297b55dd3af
rjkush17 <rjkush17rishabh@gmail.com>
2025-01-27 22:02:59 +0530

    lualinesetup done

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
 require("config.lazy")
--- Keybinfs for the grep telescope
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
--- For enable line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+require("nvim-configs")
+
---
+++ lua/nvim-configs.lua
@@ -0,0 +1,6 @@
+-- Keybinfs for the grep telescope
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+-- For enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
