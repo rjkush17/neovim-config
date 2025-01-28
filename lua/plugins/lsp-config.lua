a5e0f589275e7568de875ad86e1cf32435aac918
rjkush17 <rjkush17rishabh@gmail.com>
2025-01-28 07:33:33 +0530

    nvim lsp config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,37 @@
+return {
+	{
+	  "williamboman/mason.nvim",
+	  config = function()
+		require("mason").setup()
+	  end
+	},
+	{
+	  "williamboman/mason-lspconfig.nvim",
+	  config = function()
+		require("mason-lspconfig").setup({
+		  ensure_installed = { "lua_ls", "ts_ls", "solargraph" } -- Add other LSPs as needed
+		})
+	  end
+	},
+	{
+	  "neovim/nvim-lspconfig",
+	  config = function()
+		local lspconfig = require("lspconfig") -- Correct variable name
+		
+		-- Lua LSP setup
+		lspconfig.lua_ls.setup({})
+		
+		-- TypeScript/JavaScript LSP setup
+		lspconfig.ts_ls.setup({})
+		
+		-- Ruby LSP setup
+		lspconfig.solargraph.setup({})
+		
+		-- Key mappings for LSP functionality
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "LSP Hover" }) -- Fix typo: 'k' -> 'K'
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to Definition" })
+		vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Action" })
+	  end
+	}
+  }
+  
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+	{
       'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
@@ -6,5 +7,19 @@ return {
 		vim.keymap.set('n','<C-p>', builtin.find_files,{})
 		vim.api.nvim_set_keymap('n', '<leader>fg', ":Telescope live_grep<CR>", { noremap = true, silent = true })
 	end
+	},
+	{
+	'nvim-telescope/telescope-ui-select.nvim',
+	config = function()
+	require("telescope").setup {
+	extensions = {
+	["ui-select"] = {
+	require("telescope.themes").get_dropdown {
+      }
+	}
+	}
+	}
+	require("telescope").load_extension("ui-select")
+	end
+	}
 }
-
